0274813716037e1b8375ccd8a7e5c98cc46b2e40
ccllaarraa <clarapasteau@gmail.com>
2022-09-07 12:42:51 +0200

    update

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ gsap.registerPlugin(ScrollTrigger);
 // on click
  function startAnim()  {
         gsap.to('.logo--left', {
-            x:-265,
-            y:-187,
+            top: 30,
+            left: 30,
             duration: 0.5,
             start: "top top",
             end: "+=100%",
-            ease: "none"
+            ease: "none", 
         });
         gsap.to('.logo--right', {
-            x:370,
-            y:190,
+            bottom: 30,
+            right: 30,
             duration: 0.5,
             ease: "none",
             start: "top top",
@@ -38,6 +38,13 @@ gsap.registerPlugin(ScrollTrigger);
         var video = document.getElementById("videoid");
         video.className += " bigger";
 
+        var left = document.getElementById("logoleft");
+        left.className += " fixed";
+
+        var right = document.getElementById("logoright");
+        right.className += " fixed";
+
+
  }
 
 
@@ -46,14 +53,14 @@ gsap.registerPlugin(ScrollTrigger);
 gsap.to(".hiddenImage", {
     scrollTrigger: {
         trigger: ".hiddenImage",
-        start: "70%+=5px center ",
-        end: "bottom 200px",
+        start: "70%+=5px center+=100px ",
+        end: "70%+=15px center",
         toggleActions: "restart pause reverse reset",
+        scrub: true,
     },
     duration: 0.3,
-    scale: 0.7,
+    scale: 0.5,
     opacity: 0,
-
     filter:"blur(4px)"
 });
 
@@ -66,11 +73,10 @@ gsap.to(".text--content", {
         scrub: true,
         toggleActions: "restart pause reverse reset"
     },
-    duration: 3,
+    duration: 2,
     scale: 1.3,
     opacity: 1
 });
-
 
 
 gsap.to(".logo--left", {
@@ -79,13 +85,15 @@ gsap.to(".logo--left", {
         start: "center center",
         end: "bottom top-=10px",
         endTrigger:"footer",
-        onEnter: () => $(".logo--left").addClass('revealed').removeClass('revealedback'),
-        onLeave: () => $(".logo--left").removeClass('revealed').addClass('revealedback'),
-        onEnterBack: () => $(".logo--left").addClass('revealed').removeClass('revealedback'),
-        onLeaveBack: () => $(".logo--left").removeClass('revealed').addClass('revealedback'),
+        duration: 0.5,
+        // onEnter: () => $(".logo--left").addClass('revealed').removeClass('revealedback'),
+        // onLeave: () => $(".logo--left").removeClass('revealed').addClass('revealedback'),
+        // onEnterBack: () => $(".logo--left").addClass('revealed').removeClass('revealedback'),
+        // onLeaveBack: () => $(".logo--left").removeClass('revealed').addClass('revealedback'),
     }
  
 });
+
 
 
 gsap.to(".logo--right", {
@@ -94,10 +102,11 @@ gsap.to(".logo--right", {
         start: "center center",
         end: "bottom top-=10px",
         endTrigger:"footer",
-        onEnter: () => $(".logo--right").addClass('revealedright').removeClass('revealedrightback'),
-        onLeave: () => $(".logo--right").removeClass('revealedright').addClass('revealedrightback'),
-        onEnterBack: () => $(".logo--right").addClass('revealedright').removeClass('revealedrightback'),
-        onLeaveBack: () => $(".logo--right").removeClass('revealedright').addClass('revealedrightback'),
+
+        // onEnter: () => $(".logo--right").addClass('revealedright').removeClass('revealedrightback'),
+        //  onLeave: () => $(".logo--right").removeClass('revealedright').addClass('revealedrightback'),
+        //  onEnterBack: () => $(".logo--right").addClass('revealedright').removeClass('revealedrightback'),
+        //  onLeaveBack: () => $(".logo--right").removeClass('revealedright').addClass('revealedrightback'),
     }
  
 });
